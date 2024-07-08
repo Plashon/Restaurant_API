1794feb139ebe3bcb1f655fccc6f173736e8c0af
Plashon <fourza12poobate@gmail.com>
2024-07-08 16:56:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+app.get("/",(req,res) => {
+    res.send("<h1>Hello restaurant api</h>")
+});
+app.listen(PORT, ()=>{
+    console.log("Listen to http://localhost:"+PORT);
+})

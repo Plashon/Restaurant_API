fdb4e7743a947ce21fdc6c0ac1b090e247e6043c
Plashon <fourza12poobate@gmail.com>
2024-07-11 14:09:04 +0700

    .env gitignore

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-
+require("dotenv").config({path:"../.env"});
+// console.log(process.env.dialect);
 module.exports = {
-    HOST: "ep-square-night-a1yhcsnz-pooler.ap-southeast-1.aws.neon.tech",
-    USER: "default",
-    PASSWORD:"etzSGy0smAE9",
-    DB:"verceldb",
-    dialect: "postgres",
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD:process.env.PASSWORD,
+    DB:process.env.DB,
+    dialect:process.env.dialect,
     pool:{
         max:5,
         min:0,
